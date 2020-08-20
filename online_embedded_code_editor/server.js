2c93064a50eb3670e7222a62fcd77387d00b4891
smallpondtom <usasmpdtom@gmail.com>
2020-08-20 19:29:28 -0400

    Make changes to server

--- online_embedded_code_editor/server.js
+++ online_embedded_code_editor/server.js
@@ -1,46 +1,65 @@
-var http = require("http"),
-    url = require("url"),
-    path = require("path"),
-    fs = require("fs")
-    port = process.argv[2] || 8888;
+const express = require("express");
+const bodyParser = require("body-parser");
+const router = express.Router();
+const app = express();
+var PORT = 8888;
 
-http.createServer(function(request, response) {
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-  var uri = url.parse(request.url).pathname
-    , filename = path.join(process.cwd(), uri);
+app.get('/',(req, res) => {
+    res.sendFile(__dirname + "/editor.html");
+});
 
-  var contentTypesByExtension = {
-    '.html': "text/html",
-    '.css':  "text/css",
-    '.js':   "text/javascript"
-  };
+app.post('/compilecode' , function (req , res ) {
+    
+	var code = req.body.code;	
+	var input = req.body.input;
+    var inputRadio = req.body.inputRadio;
+    var lang = req.body.lang;
 
-  fs.exists(filename, function(exists) {
-    if(!exists) {
-      response.writeHead(404, {"Content-Type": "text/plain"});
-      response.write("404 Not Found\n");
-      response.end();
-      return;
-    }
+    console.log("hello: ", code)
+    console.log("input: ", input)
+    console.log("inputRadio: ", inputRadio)
+    console.log("lang: ", lang)
 
-    if (fs.statSync(filename).isDirectory()) filename += '/editor.html';
+    // https://github.com/tarungarg546/HackerEarth-node
+	var hackerEarth=require('hackerearth-node'); //require the Library
+	//Now set your application 
+	var hackerEarth=new hackerEarth(
+		'13a3dcad1ec898bce32526ff5d52c0b02f8cba71',  //Your Client Secret Key here this is mandatory
+		''  //mode sync=1 or async(optional)=0 or null async is by default and preferred for nodeJS
+	);
+	var config={};
+	config.time_limit=1;  //your time limit in integer
+	config.memory_limit=323244;  //your memory limit in integer
+	config.source=code;  //your source code for which you want to use hackerEarth api
+	config.input='';  //input against which you have to test your source code
+	config.language=lang; //optional choose any one of them or none
 
-    fs.readFile(filename, "binary", function(err, file) {
-      if(err) {        
-        response.writeHead(500, {"Content-Type": "text/plain"});
-        response.write(err + "\n");
-        response.end();
-        return;
-      }
+    var result = "";
+	//compile your code 
+	hackerEarth.run(config,function(err,response){
+		if(err) {
+		//deal with error
+		console.log("error: ",err)
+		} else {
+        console.log("result: ", response.substring(response.search("output_html") + 15 , response.search("memory_limit") - 4));
+        var result = response.substring(response.search("output_html") + 15 , response.search("memory_limit") - 4);
+        res.send(html_text1 + result + html_text2);
+		}
+    }); 
+    
+    // res.send(html_text1 + code + html_text2);
 
-      var headers = {};
-      var contentType = contentTypesByExtension[path.extname(filename)];
-      if (contentType) headers["Content-Type"] = contentType;
-      response.writeHead(200, headers);
-      response.write(file, "binary");
-      response.end();
+});
+
+app.get('/fullStat' , function(req , res ){
+    compiler.fullStat(function(data){
+        res.send(data);
     });
-  });
-}).listen(parseInt(port, 10));
+});
 
-console.log("Server at => http://localhost:" + port);
+app.listen(PORT, () => {
+    console.log("Server at => http://localhost:" + PORT);
+});
